4e4cd7f954628c5e74086ea78f42cd9e947d8788
lakshmiazure <108043817+lakshmiazure@users.noreply.github.com>
2022-06-28 09:19:58 +0530

    Update provider.tf

--- Folder/provider.tf
+++ Folder/provider.tf
@@ -1,8 +1,12 @@
+terraform{
+    required_version= ">=0.12"
+}
+
 provider "azurerm"{
     version= "~>2.0"
-    subscription_id= "d87f7297-c65a-4c12-9f32-466c4b10110d"
-    tenant_id= "2f9733f1-8f9e-4388-96a3-54fa03040086"
-    client_id= "33d8ab0d-f314-44c1-b591-a702dc7d9f11"
+    #subscription_id= "d87f7297-c65a-4c12-9f32-466c4b10110d"
+    #tenant_id= "2f9733f1-8f9e-4388-96a3-54fa03040086"
+    #client_id= "33d8ab0d-f314-44c1-b591-a702dc7d9f11"
     features{
     }
 
