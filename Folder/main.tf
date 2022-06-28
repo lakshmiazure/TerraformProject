52d6bbeffe2a8f8ee05a5ff7e55dc8744c1be2a4
lakshmiazure <108043817+lakshmiazure@users.noreply.github.com>
2022-06-28 08:53:50 +0530

    Update main.tf

--- Folder/main.tf
+++ Folder/main.tf
@@ -1,11 +1,11 @@
-provider "azurerm"{
-    version= "~>2.0"
+#provider "azurerm"{
+    #version= "~>2.0"
     #subscription_id= "d87f7297-c65a-4c12-9f32-466c4b10110d"
     #tenant_id= "2f9733f1-8f9e-4388-96a3-54fa03040086"
     #client_id= "33d8ab0d-f314-44c1-b591-a702dc7d9f11"
-    features{}
+    #features{}
 
-}
+#}
 #RG Creation
 resource "azurerm_resource_group" "R1"{
     name=var.rgname
